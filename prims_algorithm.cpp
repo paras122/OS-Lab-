d48acb5945e9a4c9f17708b3ce5188f63db8995c
paras122 <82634952+paras122@users.noreply.github.com>
2024-04-18 12:15:35 +0530

    Update prims_algorithm.cpp

--- prims_algorithm.cpp
+++ prims_algorithm.cpp
@@ -1,48 +1,77 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <queue>
+#include <utility>
+
+using namespace std;
 
 const int MAXN = 10;
-int parent[MAXN], rank_arr[MAXN];
-int find(int x) {
-    if (parent[x] == x) return x;
-    return parent[x] = find(parent[x]);
-}
-void union_sets(int x, int y) {
-    x = find(x);
-    y = find(y);
-    if (x == y) return;
-    if (rank_arr[x] < rank_arr[y]){
-        int tmp=x;
-        x=y;
-        y=tmp;
-    };
+const int INF = 1e9; // Infinity
+
+int graph[MAXN][MAXN];
+bool visited[MAXN];
+
+int prim(int start, int n) {
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+    }
     
-    parent[y] = x;
-    if (rank_arr[x] == rank_arr[y]){
-        rank_arr[x]++   
-    };
+    int mst_weight = 0;
+    int count=0;
+    int min = INF;
+    int temp= start;
+    visited[start]=true;
+   bool flag=false;
+
+    while(count<=n-1){
+        flag=false;
+        for (int j = 1; j <= n; j++){
+            if(!visited[j]){
+                if(graph[start][j]<min||graph[j][start]<min){
+                    min=graph[start][j];
+                    temp=j;
+                    flag= true;
+                }
+
+            }
+        }
+        if(flag)
+        mst_weight+= min;
+        cout<<mst_weight;
+        visited[temp] = true;
+        start = temp;
+        count++;
+    }
+
+    return mst_weight;
 }
+
 int main() {
     int n, m;
-    std::cout<<"Enter no. of vertices ";
-    std::cin >>n;
-    std::cout<<"Enter no. of edges ";
-    std::cin>>m;
-    for (int i = 1; i <= n; i++) {
-        parent[i] = i;
-        rank_arr[i] = 1;
-    }
-    int u, v, w;
-    for (int i = 1; i <= m; i++) {
-        std::cin >> u >> v >> w;
-        union_sets(u, v);
-    }
-    int mst_weight = 0;
-    for (int i = 1; i <= n; i++) {
-        if (parent[i] == i) {
-            mst_weight += w;
-            std::cout << "Edge: " << i << " - " << parent[i] << " Weight: " << w << std::endl;
+    cout << "Enter number of vertices: ";
+    cin >> n;
+    cout << "Enter number of edges: ";
+    cin >> m;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            graph[i][j] = INF;
         }
     }
-    std::cout << "Total MST Weight: " << mst_weight << std::endl;
+
+    for (int i = 0; i < m; i++) {
+        int u, v, w;
+        cout << "Enter edge " << i + 1 << " (u v w): ";
+        cin >> u >> v >> w;
+        graph[u][v] = w;
+        graph[v][u] = w; // Undirected graph
+    }
+
+    int start_vertex;
+    cout << "Enter starting vertex: ";
+    cin >> start_vertex;
+
+    int mst_weight = prim(start_vertex, n);
+    cout << "Total MST Weight: " << mst_weight << endl;
+
     return 0;
 }
